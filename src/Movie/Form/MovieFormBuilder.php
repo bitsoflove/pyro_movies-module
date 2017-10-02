73a5e5a8c18cad3253b182f72a9b9d922e01d82b
Michiel <michiel@Michiels-MacBook-Pro-2.local>
2017-10-02 10:22:39 +0200

    fix field + form hander + clean form handler

--- src/Movie/Form/MovieFormBuilder.php
+++ src/Movie/Form/MovieFormBuilder.php
@@ -10,55 +10,16 @@ class MovieFormBuilder extends FormBuilder
      *
      * @var array|string
      */
-    protected $fields = [];
+    protected $fields = MovieFormFields::class;
 
-    /**
-     * Additional validation rules.
-     *
-     * @var array|string
-     */
-    protected $rules = [];
-
-    /**
-     * Fields to skip.
-     *
-     * @var array|string
-     */
-    protected $skips = [];
-
-    /**
-     * The form actions.
-     *
-     * @var array|string
-     */
-    protected $actions = [];
-
-    /**
-     * The form buttons.
-     *
-     * @var array|string
-     */
-    protected $buttons = [];
-
-    /**
-     * The form options.
-     *
-     * @var array
-     */
-    protected $options = [];
 
     /**
      * The form sections.
      *
-     * @var array
+     * @var array|string
      */
-    protected $sections = [];
+    protected $sections = MovieFormSections::class;
 
-    /**
-     * The form assets.
-     *
-     * @var array
-     */
-    protected $assets = [];
+
 
 }
