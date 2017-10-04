ead399ba2027c43e608359f518201f7f1e99c2f5
Michiel <michiel@Michiels-MacBook-Pro-2.local>
2017-10-04 15:10:45 +0200

    add auto slug + skip slug in form

--- src/Movie/MovieObserver.php
+++ src/Movie/MovieObserver.php
@@ -1,8 +1,20 @@
 <?php namespace Bitsoflove\MoviesModule\Movie;
 
+use Anomaly\Streams\Platform\Entry\Contract\EntryInterface;
 use Anomaly\Streams\Platform\Entry\EntryObserver;
 
 class MovieObserver extends EntryObserver
 {
 
+    /**
+     * Run before a record is created.
+     *
+     * @param EntryInterface $entry
+     */
+    public function creating(EntryInterface $entry)
+    {
+
+        $entry->setAttribute('slug', str_slug($entry->title, '_'));
+        parent::creating($entry);
+    }
 }
